7176953d668ffb544ac0163f7742098d0ccd20df
pshriya01 <shriya.shriya16@gmail.com>
2024-01-05 18:19:28 +0530

    done

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Bar,Line } from 'react-chartjs-2';
 // import {BarElement} from 'react-chartjs-2'
 import chartData from './ChartData'; 
 import {
@@ -39,6 +39,31 @@ const BarChart = () => {
   const handleSelectChange = (event) => {
     setSelectedValue(event.target.value);
   };
+  const chartOptions = {
+    scales: {
+      x: {
+        type: 'category',
+        labels: selectedValue.map(item => item.Month),
+      },
+    y1: {
+        type: 'linear',
+        beginAtZero: true,
+        position: 'left',
+        min:0,
+        max:1000,
+        stepSize:100
+      },
+      y2: {
+        type: 'linear',
+        beginAtZero: true,
+        position: 'right',
+        min:0,
+        max:25,
+        stepSize:5
+      },
+
+    },
+  };
 
   return (
     <div>
@@ -49,25 +74,33 @@ const BarChart = () => {
       </select> */}
 
       <Bar  data={{
-          labels: selectedValue.map(item => item.Date),
+        
           datasets: [
             {
-              label: 'Emissions',
+              label: 'Emissions-2023',
               data: selectedValue.map(item => item.Emissions),
               backgroundColor: 'rgba(75,192,192,0.2)',
               borderColor: 'rgba(75,192,192,1)',
               borderWidth: 1,
+              type:"bar",
+              yAxisID: 'y1',
             },
             {
-              label: 'Revenue',
-              data: selectedValue.map(item => item.Revenue),
-              backgroundColor: 'rgba(255,99,132,0.2)',
-              borderColor: 'rgba(255,99,132,1)',
-              borderWidth: 1,
+            label: 'Revenue',
+            data: selectedValue.map(item => item.ER),
+            type: 'line',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 2,
+            fill: false,
+            yAxisID: 'y2',
             },
             
           ],
-        }}  />
+          
+        }} 
+        options={chartOptions}
+         />
+        
     </div>
   )
 }
